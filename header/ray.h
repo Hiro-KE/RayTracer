117b1a45a7a52d07cee68b6acd997120cc762dff
hirodemon <jedfakhfekh@gmail.com>
2021-07-30 00:53:52 +0100

    Create ray.h

---
+++ header/ray.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "vec4.h"
+
+class ray
+{
+
+public:
+
+    ray(){};
+    ray(const location& origin, const vec4& direction)
+        : orig(origin), dir(direction)
+    {}
+
+    inline location origin() const {return orig;}
+    inline vec4 direction() const {return dir;}
+
+    inline location at(double t) const
+    {
+        return orig + t*dir;
+    }
+
+public:
+
+    location orig;
+    vec4 dir;
+}
